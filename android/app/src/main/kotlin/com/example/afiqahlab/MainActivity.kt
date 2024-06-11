837c397009b09c8cdb14978ce94670cf599f102c
nataliaamah <ameranatalia@gmail.com>
2024-06-11 09:42:48 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/afiqahlab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.afiqahlab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  //root of application
+  //application
+  @override
+  
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'AssetImage',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      appBar: AppBar(
+      backgroundColor: Colors.purpleAccent,
+      title: Text('Home Page'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: 
+      Image(
+        image: AssetImage ('assets/images.jpg'),
+      )
+    ),
+    );
+  }
+}
